34d5d4f2a3aeb5ad3888b92a76784d097380b216
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-10-31 07:32:13 -0400

    updated
    Signed-off-by: jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>

--- ext/console/main.js
+++ ext/console/main.js
@@ -2,7 +2,7 @@
 https://nettleweb.com/
 
 
-/*! Copyright (C) 2024 nettleweb.com; All rights reserved. !*/
+/*! Copyright (C) 2025/2026 nettleweb.com; All rights reserved. !*/
 "use strict";
 
 (()=>{function e(n){var s=i[n];if(void 0!==s)return s.exports;var r=i[n]={id:n,loaded:!1,exports:{}};return t[n](r,r.exports,e),r.loaded=!0,r.exports}var t={215:(e,t,i)=>{function n(){var e=a;a=null,e&&e.forEach((function(e){s(e[0],e[1])}))}function s(e,i,s){if("undefined"!=typeof document){if(a)if(s)n();else if(!1===s)return a.push([e,i]);if(!r){var o=s;s&&s.getRootNode&&(o=s.getRootNode())&&o!=s||(o=document);var l=o.ownerDocument||o;if(i&&t.hasCssString(i,o))return null
